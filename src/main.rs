8bf16dc2a2e7efc132cdb909775b6578785e6dee
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-07-04 12:37:08 +0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s = String::from("Hello, world!");
+    println!("Hello, world!");
+    println!("{}", s);
+}
+
+fn word_tp_pig_latin(word: String) -> String{
+    String::new()
+}
