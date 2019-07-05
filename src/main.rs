8fc7424d282c4cf85a7afb4b1c3bba9549d33215
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-07-05 17:53:09 +0600

    in progress

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ fn main() {
     let mut s = String::from("Hello, world!");
     println!("Hello, world!");
     println!("{}", s);
+    println!("{}", s);
+    string_to_word(&s[0..], 0);
 }
 
 fn word_tp_pig_latin(word: &str) -> &str{
+    
+    word
+}
+
+fn string_to_word(sentence: &str, begin: u32) -> (&str, u32, u32){
     let dividers = (
         ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-',
         '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_',
         '`', '{', '|', '}', '~');
-    word
+    for letter in sentence.chars() {
+        println!("{}", letter);
+    }
+    ("122", 12, 34)
 }
